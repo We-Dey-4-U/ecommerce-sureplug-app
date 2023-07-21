56625305560442ca60fc85b7d4774d6fe5e5ad57
ikenna <mrteks2022@gmail.com>
2023-07-21 06:58:15 -0700

    done

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors({
   origin: 'http://localhost:3000',
@@ -12,6 +13,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname,"./uploads")));
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
