3ff68d0c65d57ff5bff5b94e68c7e01c04378f24
ikenna <mrteks2022@gmail.com>
2023-07-21 07:01:27 -0700

    done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://ecommerce-sureplug-app-8v1t.vercel.app//api/v2";
+export const server = "https://ecommerce-sureplug-app-8v1t.vercel.app/api/v2";
 //export const server = "http://localhost:3000"
 
 //export const backend_url = "http://localhost:3000/";
