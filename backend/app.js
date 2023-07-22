31c1ae58f16750bbcbc27dc9e905b3913714413f
ikenna <mrteks2022@gmail.com>
2023-07-21 23:27:52 -0700

    trying express instead of bodyparser

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 
@@ -18,8 +18,8 @@ app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
-//app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: true, limit: "20mb" }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
