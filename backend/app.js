b4a6694e0d59faa36bea232d85029f2f4ae03526
ikenna <mrteks2022@gmail.com>
2023-07-22 11:39:45 -0700

    params

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,8 @@ app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb", parameterLimit:50000 }));
+app.use(bodyParser.json({ extended: true, limit: '20mb'}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "20mb", parameterLimit:100000 }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
