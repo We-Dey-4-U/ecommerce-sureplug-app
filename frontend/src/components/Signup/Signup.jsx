8bdbda559fc5e847bec98e9ea5736ab968cffdd8
ikenna <mrteks2022@gmail.com>
2023-12-30 10:32:56 -0800

    ency

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -52,7 +52,7 @@ const Singup = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" onSubmit={handleSubmit}>
+          <form className="space-y-6" onSubmit={handleSubmit} encType="multipart/form-data">
             <div>
               <label
                 htmlFor="email"
