af76365879388677c09bd933246e5a31f20518e3
ikenna <mrteks2022@gmail.com>
2024-02-19 22:46:36 -0800

    dependencies update

--- backend/controller/payment.js
+++ backend/controller/payment.js
@@ -9,9 +9,9 @@ router.post(
   catchAsyncErrors(async (req, res, next) => {
     const myPayment = await stripe.paymentIntents.create({
       amount: req.body.amount,
-      currency: "inr",
+      currency: "$",
       metadata: {
-        company: "Becodemy",
+        company: "Codecraft",
       },
     });
     res.status(200).json({
